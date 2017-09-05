db2aea5e91260f865f2958a36429a0db1847f182
luca16s <luca.gian16@outlook.com>
2017-09-04 21:09:36 -0300

    First commit

---
+++ sonda/Sonda.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace sonda
+{
+    public class Sonda
+    {
+        public Sonda(int yCoordinate, char direction, int xCoordinate)
+        {
+            this._yCoordinate = yCoordinate;
+            this._direction = direction;
+            this._xCoordinate = xCoordinate;
+        }
+
+        private readonly int _xCoordinate;
+        public int XCoordinate { get; set; }
+
+        private readonly int _yCoordinate;
+        private int YCoordinate { get; set; }
+
+        private readonly char _direction;
+        private char Direction { get; set; }
+    }
+}
