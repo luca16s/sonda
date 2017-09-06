8703f84f86731110cd2841066c5b556c4f53ec4d
luca16s <luca.gian16@outlook.com>
2017-09-05 21:15:17 -0300

    Inclusão do Executavel

---
+++ ProjetoDePouso/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProjetoDePouso
+{
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insira as Coordenadas do Ponto Superior Direito: ");
+            string plateuSize = Console.ReadLine();
+
+            Console.WriteLine("Insira a posição inicial: ");
+            string initialPosition = Console.ReadLine();
+
+            Console.WriteLine("Insira a direção para movimentar a sonda");
+            string direction = Console.ReadLine();
+        }
+    }
+}
