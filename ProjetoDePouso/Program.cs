373aaaa8f8cd4550f2c63d812b5c5ecd473e58ec
luca16s <luca.gian16@outlook.com>
2017-09-06 16:31:36 -0300

    Implementando Async await

--- ProjetoDePouso/Program.cs
+++ ProjetoDePouso/Program.cs
@@ -1,36 +1,84 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using sonda;
 
 namespace ProjetoDePouso
 {
-    internal static class Program
+    internal static class ControlaImpressaoAsync
     {
         private static void Main(string[] args)
         {
+            var plateuSize = "";
+            var initialPosition = new List<string>();
+            var x = new List<string>();
+            var y = new List<string>();
+            var position = new List<string>();
+            var direction = new List<string>();
+            IMovementable sondaA = new Sonda();
+            IMovementable sondaB = new Sonda();
+            (int, int, string) valueSondaA = (0, 0, null);
+            (int, int, string) valueSondaB = (0, 0, null);
+
+            Console.Title = "NASA - Programa Espacial";
             Console.WriteLine("Insira as Coordenadas do Ponto Superior Direito: ");
-            var plateuSize = Console.ReadLine();
+            plateuSize = Console.ReadLine();
 
-            Console.WriteLine("Insira a posição inicial: ");
-            var initialPosition = Console.ReadLine().ToUpper();
+            Console.WriteLine("Insira a posição inicial das sondas: ");
+            initialPosition.Add(Console.ReadLine().ToUpper());
+            initialPosition.Add(Console.ReadLine().ToUpper());
 
-            Console.WriteLine("Insira a direção para movimentar a sonda");
-            var direction = Console.ReadLine().ToUpper();
+            int i;
+            for (i = 0; i < 2; i++)
+            {
+                x.Add(initialPosition[i].Substring(0, 1));
+                y.Add(initialPosition[i].Substring(1, 1));
+                position.Add(initialPosition[i].Substring(2, 1));
 
-            
-            IMovementable movement = new Sonda();
-            var x = initialPosition.Substring(0, 1);
-            var y = initialPosition.Substring(1, 1);
-            var position = initialPosition.Substring(2, 1);
+                if (Convert.ToInt32(x[i]) > Convert.ToInt32(plateuSize.Substring(0, 1)) ||
+                    Convert.ToInt32(y[i]) > Convert.ToInt32(plateuSize.Substring(1, 1)))
+                {
+                    initialPosition.RemoveRange(i, 1);
+                    Console.WriteLine(
+                        "Reinsira a posição inicial, a mesma não pode ser maior que o tamanho do planalto: ");
+                    initialPosition.Add(Console.ReadLine().ToUpper());
+                }
+                else
+                {
+                    if (initialPosition.Count == 2)
+                    {
+                        Console.WriteLine("Insira a direção para movimentar a sonda");
+                        for (i = 0; i < 2; i++)
+                        {
+                            direction.Add(Console.ReadLine().ToUpper());
+                        }
+                    }
+                }
+            }
+            valueSondaA = ImpressaoAsync(valueSondaA, sondaA, position, x, y, direction, 0).Result;
+            valueSondaB = ImpressaoAsync(valueSondaB, sondaB, position, x, y, direction, 1).Result;
 
-            (int, int, string) value;
-
-            value = movement.North(position, x, y, direction);
-            value = movement.South(position, x, y, direction);
-            value = movement.East(position, x, y, direction);
-            value = movement.West(position, x, y, direction);
-
-            Console.WriteLine($"{value.Item1} {value.Item2} {value.Item3}");
+            Console.WriteLine("A posição da sonda passou a ser:");
+            Console.WriteLine($"{valueSondaA.Item1} {valueSondaA.Item2} {valueSondaA.Item3}");
+            Console.WriteLine($"{valueSondaB.Item1} {valueSondaB.Item2} {valueSondaB.Item3}");
             Console.ReadLine();
         }
+
+        private static (int, int, string) MostraPosicao((int, int, string) valueSonda, IMovementable sonda, IReadOnlyList<string> position, IReadOnlyList<string> x, List<string> y, IReadOnlyList<string> direction, int controller)
+        {
+            valueSonda = sonda.North(position[controller], x[controller], y[controller], direction[controller]);
+            valueSonda = sonda.South(position[controller], x[controller], y[controller], direction[controller]);
+            valueSonda = sonda.East(position[controller], x[controller], y[controller], direction[controller]);
+            valueSonda = sonda.West(position[controller], x[controller], y[controller], direction[controller]);
+            return valueSonda;
+        }
+
+        private static async Task<(int, int, string)> ImpressaoAsync((int, int, string) valueSonda, IMovementable sonda,
+            List<string> position, List<string> x, List<string> y, List<string> direction, int controller)
+        {
+            var  value = await Task.Run(() => MostraPosicao(valueSonda, sonda, position, x, y, direction, controller));
+            return value; 
+        } 
     }
 }
